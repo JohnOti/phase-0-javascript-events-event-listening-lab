f1bdc5fc30e23fbf62b3c6b64011b502138868f5
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-04 18:46:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const butt = document.getElementById('button');
 function addingEventListener() {
+    alert('I was clicked!');
+    
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+
+butt.addEventListener('click', addingEventListener);
 }
